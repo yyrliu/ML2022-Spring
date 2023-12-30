b80c2b4775fef94d73a10395cc14145c0816cc7e
Daniel Liu <70476688+yyrliu@users.noreply.github.com>
2023-12-30 15:28:31 +0100

    Add inference module

---
+++ HW06/inference.py
@@ -0,0 +1,72 @@
+import argparse
+from pathlib import Path
+from shutil import rmtree
+
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+
+import config as cfg
+from model import Generator
+from utils import fix_random_seed, load_config, setup_logger
+
+
+def inference(
+    checkpoint=None, n_generate=1000, n_output=100, show=False, overwrite=False
+):
+    """
+    1. G_path is the path for Generator ckpt
+    2. You can use this function to generate final answer
+    """
+
+    logger = setup_logger("hw6.gan", use_wandb=False)
+
+    output_dir = Path(cfg.config.workspace_dir, "output")
+
+    try:
+        Path(output_dir).mkdir()
+    except FileExistsError as e:
+        if not overwrite:
+            logger.error("Output directory exists! Inference aborted.")
+            raise e
+        logger.warning("Output will be overwritten...")
+        rmtree(output_dir)
+        Path(output_dir).mkdir()
+
+    if checkpoint is None:
+        checkpoint = sorted(
+            Path(cfg.config.ckpt_dir).glob("G_*.pt"),
+            key=lambda x: int(x.stem.split("_")[-1]),
+            reverse=True,
+        )[0]
+
+    logger.info(f"Loading checkpoint from {checkpoint}")
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    generator = Generator(cfg.config.z_dim).to(device)
+    generator.load_state_dict(torch.load(checkpoint))
+    generator.eval()
+    z_samples = torch.randn(n_generate, cfg.config.z_dim).to(device)
+    with torch.no_grad():
+        imgs = (generator(z_samples) + 1) / 2.0
+
+    for i, img in enumerate(imgs):
+        torchvision.utils.save_image(img, Path(output_dir, f"{i+1:03}.jpg"))
+
+    if show:
+        row, col = n_output // 10, 10
+        grid_img = torchvision.utils.make_grid(imgs[:n_output].cpu(), nrow=row)
+        plt.figure(figsize=(row, col))
+        plt.imshow(grid_img.permute(1, 2, 0))
+        plt.show()
+
+
+if __name__ == "__main__":
+    fix_random_seed(2022)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", required=True, type=str, default="config.py")
+    parser.add_argument("--force", action="store_true")
+    args = parser.parse_args()
+
+    load_config(args.config)
+    inference(show=True, overwrite=args.force)
