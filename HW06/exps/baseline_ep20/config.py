0f3d1304f1645c50ffebf8482b634a013863caef
Daniel Liu <70476688+yyrliu@users.noreply.github.com>
2024-01-04 13:58:54 +0100

    Add new exps

---
+++ HW06/exps/baseline_ep20/config.py
@@ -0,0 +1,16 @@
+config = {
+    "model_type": "GAN",
+    "batch_size": 64,
+    "lr": 1e-4,
+    "n_epoch": 20,
+    "n_critic": 1,
+    "z_dim": 100,
+    "workspace_dir": None,
+    "data_dir": "./data/faces",
+    "use_wandb": True,
+    "log_step": 50,
+    "valid_fid_set": "animeface_2000",
+    "valid_afd_thres": 0.895,
+}
+
+arch_args = {}
