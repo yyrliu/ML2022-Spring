7c9402599f12dccd7a7107de8d795bc12e35b651
Daniel Liu <70476688+yyrliu@users.noreply.github.com>
2024-01-07 09:16:09 +0100

    Add wgan exps

---
+++ HW06/exps/wgan_wc001_lr1e-5_ncr2/config.py
@@ -0,0 +1,17 @@
+config = {
+    "model_type": "WGAN",
+    "batch_size": 64,
+    "lr": 1e-5,
+    "weight_clip": 0.01,
+    "n_epoch": 30,
+    "n_critic": 2,
+    "z_dim": 100,
+    "workspace_dir": None,
+    "data_dir": "./data/faces",
+    "use_wandb": True,
+    "log_step": 50,
+    "valid_fid_set": "animeface_2000",
+    "valid_afd_thres": 0.895,
+}
+
+arch_args = {}
---
+++ HW06/exps/wgan_wc001_lr5e-5_ncr5/config.py
@@ -0,0 +1,17 @@
+config = {
+    "model_type": "WGAN",
+    "batch_size": 64,
+    "lr": 5e-5,
+    "weight_clip": 0.01,
+    "n_epoch": 50,
+    "n_critic": 5,
+    "z_dim": 100,
+    "workspace_dir": None,
+    "data_dir": "./data/faces",
+    "use_wandb": True,
+    "log_step": 50,
+    "valid_fid_set": "animeface_2000",
+    "valid_afd_thres": 0.895,
+}
+
+arch_args = {}
